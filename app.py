dc156fc8b0e9520a33c372598832923e755505ab
dan aronson <dan.aronson+git@gmail.com>
2020-04-09 00:23:45 -0700

    refactored and support first post

--- app.py
+++ app.py
@@ -1,55 +1,32 @@
 from flask import Flask, request
 import logging
-import tempfile
 import os
 import json
-import secrets
+import service
 
-# we avoid the GIL by using a process to actually keep track of the ids
-from multiprocessing.connection import Listener, Client
 
-unix_socket = tempfile.mkstemp()[1]
-unix_socket = '/tmp/f'
+
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 app.config.from_object('config')
 app.logger.setLevel(app.config['LOG_LEVEL'])
 
-auth_key = secrets.token_bytes(16)
-
-import pdb
-#pdb.set_trace()
-
-address = ('localhost', app.config['PORT'])
-
-
-try:
-    pid = os.fork()
-except OSError:
-    exit("Could not create a child process")
- 
-if pid == 0:
-    print('foobar')
-    print(unix_socket)
-    with Listener(address, authkey = auth_key) as listener:
-        while True:
-            with listener.accept() as conn:
-                print('connection accepted from', listener.last_accepted)
-                print(json.loads(conn.recv_bytes()))
-    print('barfoo')
-    exit()
+service.setup_server(app)
 
 @app.before_request
 def create_client():
     print(request.args)
-    request.client = Client(address, authkey = auth_key)
+    request.client = service.client()
     
 @app.route("/red", methods = ['POST'])
 def post_red():
-    request.client.send_bytes(json.dumps(request.json).encode())
-    return "Hello world"
+    data = request.json
+    logger.info('in POST red: %s' % data)
+    request.client.send([service.STORE_COMMAND, data])
+    return 'OK'
+
 
 if __name__ == '__main__':
-    logging.basicConfig()
+    logging.basicConfig(level=app.config['LOG_LEVEL'])
     app.run()
     
---
+++ service.py
@@ -0,0 +1,81 @@
+# the module contains the client and server process to manage ids
+
+import logging
+import os
+import json
+
+os.umask(0o007)
+
+# we avoid the GIL by using a process to actually keep track of the ids
+from multiprocessing.connection import Listener, Client
+
+import secrets
+
+logger = logging.getLogger(__name__)
+
+auth_key = secrets.token_bytes(16)
+
+address = None
+
+directory_root = None
+
+ids = set()
+
+# load_ids reads the file system for all to keep a cache of ids in memory
+def load_ids(directory):
+    for root, sub_dirs, files in os.walk(directory):
+        for file_name in files:
+            if file_name.endswith('.data'):
+                ids.add(file_name)
+    return ids
+                
+
+# Contacts are stored in a 4 level directory structure.  Such that for contact ABCDEFGHxxx, it is stored is AB/CD/EF/ABCDEFGHxxx.  Each contact is a file which contains JSON data.
+def store_id(hex_string, json_data):
+    first_level = hex_string[0:2].upper()
+    second_level = hex_string[2:4].upper()
+    third_level = hex_string[4:6].upper()
+    dir_name = "%s/%s/%s/%s" % (directory_root, first_level, second_level, third_level)
+    file_name = "%s/%s.data" % (dir_name, hex_string)
+    os.makedirs(dir_name, 0o770)
+    with open(file_name, 'w') as file:
+        json.dump(json_data, file)
+    ids.add(hex_string)
+    return
+
+def store_ids(data):
+    for contact in data['contacts']:
+        store_id(contact['id'], contact)
+    return
+
+STORE_COMMAND = 'STORE'
+command_functions = {STORE_COMMAND: store_ids}
+
+
+
+# run_server is the main server loop
+def run_server():
+    with Listener(address, authkey = auth_key) as listener:
+        while True:
+            with listener.accept() as conn:
+                try:
+                    data = conn.recv()
+                    command = data[0]
+                    logger.info('connection accepted from %s, processing command: %s' % (listener.last_accepted, command))
+                    command_functions[command](*data[1:])
+                except:
+                    logger.exception('Error in server')
+                    
+
+
+def setup_server(app):
+    global address, directory_root
+    address = ('localhost', app.config['PORT'])
+    directory_root = app.config['DIRECTORY']
+    pid = os.fork()
+    if 0 == pid:
+        run_server()
+        exit(0)
+
+def client():
+    return Client(address, authkey = auth_key)
