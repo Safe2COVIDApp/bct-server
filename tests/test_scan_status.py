04dfaa522ad34d6ffb4e6b458fc0878856bc48cd
dan aronson <dan.aronson+git@gmail.com>
2020-04-17 19:23:44 -0700

    fixed test

--- tests/test_scan_status.py
+++ tests/test_scan_status.py
@@ -1,12 +1,14 @@
 import requests
 
 
+
 def test_scan_status(server):
     server.reset()
     contact_id = "12345678"
     data = [{"id":contact_id}]
     server.send_status(contacts = data)
     resp = server.scan_status(contact_prefixes = ['123'])
+    assert resp.json()['ids'] == data
     return
 
 
